d3bbf17fa1fe41950124e301ee38634d38ea444c
joyscript <joyscript7@gmail.com>
2022-02-09 03:29:29 +0300

    feat: implement function translate content

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { addBurgerClickHandler } from './js/burger.js';
 import { renderPortfolioImages } from './js/portfolio.js';
+import { addTranslateHandler } from './js/translate.js';
 
 addBurgerClickHandler();
 renderPortfolioImages();
+addTranslateHandler();
