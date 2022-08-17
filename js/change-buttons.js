ffe42924c9db1ea93242d917093606bd71b6a976
joyscript <joyscript7@gmail.com>
2022-08-17 07:36:35 +0300

    fix: some little changes

--- js/change-buttons.js
+++ js/change-buttons.js
@@ -1,14 +1,10 @@
 export const changeButtons = () => {
-  const heroBtn = document.querySelector('.hero .button');
-
   const animateBtn = (btn) => {
     btn.classList.add('animated');
     btn.addEventListener('animationend', () => btn.classList.remove('animated'), { once: true });
   };
 
   document.addEventListener('click', (e) => {
-    if (e.target.classList.contains('button') && e.target !== heroBtn) animateBtn(e.target);
+    if (e.target.classList.contains('button')) animateBtn(e.target);
   });
-
-  heroBtn.addEventListener('click', () => (window.location = './index.html#contacts'));
 };
